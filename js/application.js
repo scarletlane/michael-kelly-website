c072215bede97a635f514b2e463ee48d88707fbc
Scarlet Lane <scarlet623@gmail.com>
2014-01-28 11:00:45 -0700

    photos page and icon edits

--- js/application.js
+++ js/application.js
@@ -7,5 +7,12 @@ $(document).ready(function() {
 		 return false;
 	});
 
-  
-});
+	var userFeed = new Instafeed({
+		get: 'user',
+        userId: 31038635,
+        accessToken: '31038635.467ede5.dc9e5064db244367b5ae6963922831c2',
+        limit: 30,
+	});
+	userFeed.run();
+
+});	
